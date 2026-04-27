8e4e8230f1add75c7a466a0564307d3709885a45
Rohan Mia <rohanmia.org@gmail.com>
2026-04-28 02:20:42 +0600

    added group routing, header, not-found page, anc changed font to poppines

---
+++ src/app/(auth)/register/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RegisterPage = () => {
+  return (
+    <div>
+      Register PAge
+    </div>
+  )
+}
+
+export default RegisterPage
--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import Image from "next/image";
+import logo from "@/assets/logo.png";
+import { format } from "date-fns";
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className="py-12 text-center">
+      <Image
+        src={logo}
+        width={470}
+        height={60}
+        alt="logo"
+        className="mx-auto mb-2"
+      />
+      <p className="text-[#706F6F] text-xl">
+        Journalism Without Fear or Favour
+      </p>
+      <p className="text-xl font-semibold">
+        <span>{format(new Date(), "EEEE,")}</span>
+        <span className="text-[#706F6F]">{format(new Date(), "MMMM dd, yyy")}</span>
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
