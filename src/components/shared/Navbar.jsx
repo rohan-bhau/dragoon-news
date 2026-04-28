dc8eca14a4ba6523c0783c12394b32abc0c2933f
Rohan Mia <rohanmia.org@gmail.com>
2026-04-29 05:01:30 +0600

    customized user info and added logout functionality

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,13 +1,19 @@
+'use client'
 import Link from 'next/link'
-import user from '@/assets/user.png';
+import avatar from '@/assets/user.png';
 import Image from 'next/image';
 import NavLink from './NavLink';
+import { authClient } from '@/lib/auth-clinet';
 
 const Navbar = () => {
+  const { data: session } = authClient.useSession();
+  const user = session?.user
+  console.log(user)
+
   return (
-    <div className='flex justify-between container mx-auto mt-5 mb-5'>
+    <div className="flex justify-between container mx-auto mt-5 mb-5">
       <div></div>
-      <ul className='flex gap-4 text-[#706f6f] font-semibold'>
+      <ul className="flex gap-4 text-[#706f6f] font-semibold">
         <li>
           <NavLink href="/">Home</NavLink>
         </li>
@@ -18,9 +24,23 @@ const Navbar = () => {
           <NavLink href="/career">Career</NavLink>
         </li>
       </ul>
-      <div className='flex justify-between items-center gap-3'>
-        <Image src={user} alt="user Avatar" width={40} height={40} />
-        <Link href="/login"><button className="btn bg-[#403F3F] text-[#FFFFFF]">Login</button></Link>
+      <div className="flex justify-between items-center gap-3">
+        {user ? (
+          <div className='flex items-center gap-3'>
+            <p className='text-lg font-semibold '>Hello! {user.name}</p>
+            <Image
+              src={user?.image ? user.image : avatar}
+              alt="user Avatar"
+              width={40}
+              height={40}
+            />
+            <button onClick={async()=>await authClient.signOut()} className="btn btn-error  text-[#FFFFFF]">Logout</button>
+          </div>
+        ) : (
+          <Link href="/login">
+            <button className="btn bg-[#403F3F] text-[#FFFFFF]">Login</button>
+          </Link>
+        )}
       </div>
     </div>
   );
